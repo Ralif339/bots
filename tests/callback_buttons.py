2e537503a88fc74e19c568b3fd692c4f51a773f5
ralif339 <yessir339@example.com>
2025-07-06 13:46:21 +0300

    s

--- tests/callback_buttons.py
+++ tests/callback_buttons.py
@@ -2,7 +2,8 @@ from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
 from aiogram.types import (InlineKeyboardButton, 
                            InlineKeyboardMarkup, 
-                           Message)
+                           Message,
+                           CallbackQuery)
 
 
 BOT_TOKEN = '6551803399:AAF5uGEulD8V28QsSuOSPMeeE3LS6p0Ozcc'
@@ -30,6 +31,28 @@ async def process_start_command(message: Message):
         reply_markup=keyboard
     )
     
+    
+@dp.callback_query(F.data == 'button_1_pressed')
+async def process_button_1_press(callback: CallbackQuery):
+    some_text = 'Была нажата кнопка 1'
+    if callback.message.text != 'Была нажата кнопка 1':
+        await callback.message.edit_text(
+            text='Была нажата кнопка 1',
+            reply_markup=callback.message.reply_markup
+            )
+    await callback.answer(text='Ура! Нажата кнопка 1',show_alert=True)
+    
+
+@dp.callback_query(F.data == 'button_2_pressed')
+async def process_button_2_pressed(callback: CallbackQuery):
+    some_text = 'Была нажата кнопка 2'
+    if callback.message.text != 'Была нажата кнопка 2':
+        await callback.message.edit_text(
+            text='Была нажата кнопка 2',
+            reply_markup=callback.message.reply_markup
+        )
+    await callback.answer(text='Ура! Нажата кнопка 2', show_alert=True)
+
 
 if __name__ == "__main__":
     dp.run_polling(bot)
