5634a928065c94ca4929316e808517d88c9ccad2
ralif <yessir339@mail.ru>
2025-07-10 15:13:50 +0300

    s

---
+++ book_bot/database/database.py
@@ -0,0 +1,6 @@
+user_dict__template = {
+    'page': 1,
+    'bookmarks': set()
+}
+
+users_db = {}
---
+++ book_bot/services/file_handling.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+
+BOOK_PATH = 'book/book.txt'
+PAGE_SIZE = 1050
+
+
+book: dict[int, str] = {}
+
+def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
+    pass
+
+
+def prepare_book(path: str):
+    pass
+
+
+prepare_book(os.path.join(os.path[0], os.path.normpath(BOOK_PATH)))
+    
